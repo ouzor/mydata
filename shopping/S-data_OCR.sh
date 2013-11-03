83c9c6430c660d490119a37d5e99e58ca8f50d39
Juuso Parkkinen <juuso.parkkinen@gmail.com>
2013-11-03 20:25:43 +0200

    updated OCR shell scripts

--- shopping/S-data_OCR.sh
+++ shopping/S-data_OCR.sh
@@ -1,28 +1,19 @@
 #!/bin/bash
 
-% Copyright (C) 2013 Juuso Parkkinen.
-% Contact: <http://ouzor.github.com/contact>. 
-% All rights reserved.
+echo "S-data: split, convert pdfs to png, run ocr"
+echo "Note! The scripts outputs a lot of warnings, but worked fine anyway for me…"
 
-% This program is open source software; you can redistribute it and/or modify
-% it under the terms of the FreeBSD License (keep this notice):
-% http://en.wikipedia.org/wiki/BSD_licenses
-
-% This program is distributed in the hope that it will be useful,
-% but WITHOUT ANY WARRANTY; without even the implied warranty of
-% MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.
-
-% This script assumes you have a scanned your S-group data into file S-data.pdf
-
-% Create folders for the output
-mkdir TEMP_PDF
-mkdir TEMP_PNG
-mkdir TEMP_TXT
-
-echo "S-data: split pdfs, convert to png, run ocr"
 for i in {1..31}
 do
-  pdftk S-data.pdf cat $i output TEMP_PDF/S-data_split_$i.pdf
-  convert -quiet -density 600 -size 210 × 297 -depth 8 TEMP_PDF/S-data_split_$i.pdf TEMP_PNG/S-data_split_$i.png
-  tesseract TEMP_PNG/S-data_split_$i.png -l fin TEMP_TXT/S-data_split_$i
+%	pdftk S-data.pdf cat $i output S-data_processed/S-data_split_$i.pdf
+%	convert -quiet -density 600 -size 210 × 297 -depth 8 S-data_processed/S-data_split_$i.pdf	S-data_processed/S-data_split_$i.png
+ 	tesseract S-data_processed/S-data_split_$i.png -l fin S-data_processed/S-data_split_$i
 done
+
+% Need to change all filename from -1.png to -01.png to preserve ordering in the next phase
+% Could be done automatically as well!
+
+% Combine all text files into one
+cat S-data_processed/*.txt >> S-data_processed/S-data_raw.txt
+
+% The data is next curated manually and then finally processed in R
